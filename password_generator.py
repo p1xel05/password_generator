f0cc9461c7c327dfc9d1487f196128fec0745dc3
p1xel05 <106555540+p1xel05@users.noreply.github.com>
2026-06-19 23:40:40 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,12 +1,18 @@
 import random as rnd
 import string
 
-alphabet = string.ascii_letters + string.digits + string.punctuation
+alphabet = string.ascii_letters
+numbers = string.digits
+punctuation = string.punctuation
 
 password = ""
 
 for i in range(10):
     password += rnd.choice(alphabet)
 
+for i in range(2):
+    password += rnd.choice(numbers)
+
+password += rnd.choice(punctuation)
 
 print(password)
